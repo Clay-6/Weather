bed7c1093a7bd6f291a41128d9a5f35146633ed2
Clayman <claytonwambu@gmail.com>
2022-03-19 22:00:07 +0000

    Can send HTTP requests (no json parsing yet)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 #![allow(non_snake_case)]
+use std::collections::HashMap;
+use std::io::{self, Write};
 
-fn main() {
-    println!("Hello, world!");
+const API_KEY: &str = "a3fa1bfa682d5b4f93efe3abd290edd1";
+const BASE_URL: &str = "http://api.openweathermap.org/data/2.5/weather";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        let mut buffer = String::new();
+        print!("Enter a city name or type 'quit' to exit: ");
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read line.");
+
+        let city = buffer.trim().to_lowercase();
+        if city == String::from("quit") {
+            break;
+        }
+
+        let request_url = format!("{}?appid={}&q={}&units=metric", BASE_URL, API_KEY, city);
+
+        let response = reqwest::blocking::get(request_url)?.text()?;
+        println!("{:?}\n", response);
+    }
+    Ok(())
 }
