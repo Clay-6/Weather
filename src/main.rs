7cfc8ad0a676cdac997c3294956c4aaeb2af7523
Clayman <claytonwambu@gmail.com>
2022-03-20 13:05:17 +0000

    Added error message for invalid query

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     const BASE_URL: &str = "http://api.openweathermap.org/data/2.5/weather";
 
     loop {
-        let mut buffer = String::new();
-        print!("Enter a city name or type 'quit' to exit: ");
+        let mut input = String::new();
+        print!("Enter a place name or type 'quit' to exit: ");
         io::stdout().flush().unwrap();
 
         io::stdin()
-            .read_line(&mut buffer)
+            .read_line(&mut input)
             .expect("Failed to read line.");
 
-        let city = buffer.trim().to_lowercase();
-        if city == String::from("quit") {
+        let location = input.trim().to_lowercase();
+        if location == String::from("quit") {
             break;
         }
 
-        let request_url = format!("{}?appid={}&q={}&units=metric", BASE_URL, API_KEY, city);
+        let request_url = format!("{}?appid={}&q={}&units=metric", BASE_URL, API_KEY, location);
         let response = reqwest::get(request_url).await?.text().await?;
 
         let data: Value = serde_json::from_str(response.as_str())?;
         let weather = &data["weather"][0]["description"];
         let temperature = &data["main"]["temp"];
 
+        if weather.is_null() || temperature.is_null() {
+            eprintln!("Data for \"{}\" not found\n", location);
+            continue;
+        }
+
         println!(
             "Weather description: {}\n\
-            Temperature: {}",
+            Temperature: {}\n",
             weather, temperature
         );
     }
