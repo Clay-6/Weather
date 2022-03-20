2db8b698a15f4aaba707275b81da394500a7dec9
Clayman <claytonwambu@gmail.com>
2022-03-20 14:48:23 +0000

    Added units to the temperature

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         println!(
             "Weather description: {}\n\
-            Temperature: {}\n",
+            Temperature: {}℃\n",
             weather, temperature
         );
     }
