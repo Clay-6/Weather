bd7a19246aa0ffa42540caa0e03925ec885d40c3
Clayman <claytonwambu@gmail.com>
2022-03-21 21:18:26 +0000

    Can now optionally use command-line args

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = env::var("WEATHER_API_KEY").unwrap();
     const BASE_URL: &str = "http://api.openweathermap.org/data/2.5/weather";
 
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        let location = &args[1];
+        let request_url = format!("{}?appid={}&q={}&units=metric", BASE_URL, api_key, location);
+        let response = reqwest::get(request_url).await?.text().await?;
+
+        let data: Value = serde_json::from_str(response.as_str())?;
+        let weather = &data["weather"][0]["description"];
+        let temperature = &data["main"]["temp"];
+
+        if weather.is_null() || temperature.is_null() {
+            eprintln!("Data for \"{}\" not found", location);
+        }
+
+        println!(
+            "Weather description: {}\n\
+        Temperature: {}⁰C\n",
+            weather, temperature
+        );
+    }
+
     loop {
         let mut input = String::new();
         print!("Enter a place name or type 'quit' to exit: ");
