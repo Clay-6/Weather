d8977ed616b843382bdbf974a2c8561cfe51ecf6
Clayman <claytonwambu@gmail.com>
2022-04-01 15:28:49 +0100

    Showing data now in its own function

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
+#![allow(non_snake_case)]
+
 use serde_json::Value;
 use std::env;
 use std::io::{self, Write};
 
+static BASE_URL: &str = "http://api.openweathermap.org/data/2.5/weather";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = env::var("WEATHER_API_KEY").unwrap();
-    const BASE_URL: &str = "http://api.openweathermap.org/data/2.5/weather";
 
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
         let location = &args[1];
-        let request_url = format!("{}?appid={}&q={}&units=metric", BASE_URL, api_key, location);
-        let response = reqwest::get(request_url).await?.text().await?;
-
-        let data: Value = serde_json::from_str(response.as_str())?;
-        let weather = &data["weather"][0]["description"];
-        let temperature = &data["main"]["temp"];
-
-        if weather.is_null() || temperature.is_null() {
-            eprintln!("Data for \"{}\" not found", location);
-        }
-
-        println!(
-            "Weather description: {}\n\
-        Temperature: {}⁰C\n",
-            weather, temperature
-        );
+        ShowData(&api_key, location).await?;
 
         std::process::exit(0);
     }
@@ -44,24 +32,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             s => String::from(s),
         };
 
-        let request_url = format!("{}?appid={}&q={}&units=metric", BASE_URL, api_key, location);
-        let response = reqwest::get(request_url).await?.text().await?;
-
-        let data: Value = serde_json::from_str(response.as_str())?;
-        let weather = &data["weather"][0]["description"];
-        let temperature = &data["main"]["temp"];
-
-        if weather.is_null() || temperature.is_null() {
-            eprintln!("Data for \"{}\" not found\n", location);
-            continue;
-        }
-
-        println!(
-            "Weather description: {}\n\
-            Temperature: {}⁰C\n",
-            weather, temperature
-        );
+        ShowData(&api_key, &location).await?;
     }
 
+    Ok(())
+}
+
+async fn ShowData(api_key: &String, location: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let request_url = format!("{}?appid={}&q={}&units=metric", BASE_URL, api_key, location);
+    let response = reqwest::get(request_url).await?.text().await?;
+    let data: Value = serde_json::from_str(response.as_str())?;
+    let weather = &data["weather"][0]["description"];
+    let temperature = &data["main"]["temp"];
+    if weather.is_null() || temperature.is_null() {
+        eprintln!("Data for \"{}\" not found", location);
+    }
+    println!(
+        "Weather description: {}\n\
+        Temperature: {}⁰C\n",
+        weather, temperature
+    );
     Ok(())
 }
