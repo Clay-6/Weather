9929bed98f23341a1e6e33510a872b759f2accda
Clayton <claytonwambu@gmail.com>
2022-04-25 23:03:18 +0100

    Now uses anyhow to handle errors in `get_data`

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
+use anyhow::{anyhow, Result};
 use serde_json::Value;
 
-pub async fn get_data(
-    base_url: &str,
-    api_key: &str,
-    location: &str,
-) -> Result<(String, String), Box<dyn std::error::Error>> {
+/// Retrieves and returns the weather data from the given url using the
+/// API key
+pub async fn get_data(base_url: &str, api_key: &str, location: &str) -> Result<(String, String)> {
     let request_url = format!("{}?appid={}&q={}&units=metric", base_url, api_key, location);
     let response = reqwest::get(request_url).await?.text().await?;
     let data: Value = serde_json::from_str(response.as_str())?;
@@ -12,7 +11,8 @@ pub async fn get_data(
     let temperature = &data["main"]["temp"];
     if weather.is_null() || temperature.is_null() {
         eprintln!("Data for \"{}\" not found", location);
+        Err(anyhow!("Weather data could not be found"))
+    } else {
+        Ok((temperature.to_string(), weather.to_string()))
     }
-
-    Ok((temperature.to_string(), weather.to_string()))
 }
